f393a569a9e63e5942f36b850b45e391befbbb47
mamoun kamal alshishani <82365853+MAMOUN-kamal-alshisani@users.noreply.github.com>
2021-05-01 17:04:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+// Write your code here
 function sum(a, b) { //eslint-disable-line
-let output = a + b
-return [output, `The sum of ${a} and ${b} is ${output}.`]
+    let sumResult =a+b;
+    return [sumResult,`The sum of ${a} and ${b} is ${sumResult}`;
 }
+ 
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -24,13 +26,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-let out = a * b
-return [out, `The product of ${a} and ${b} is ${out}.`]
+let multi = a * b
+return [multi, `The product of ${a} and ${b} is ${multi}.`]
 }
-
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
-
+ testMultiply(5,9);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -49,7 +49,32 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
     let multAbc = multiply(multAb,c)[0];
     return [sumAbc , multAbc ,`${a} and ${b} and ${c} sum to ${sumAbc}.` ,`The product of ${a} and ${b} and ${c} is ${multAbc}.` ]
 }
+// Write your code here
+// Write your code here
+function sum(a, b) { //eslint-disable-line
+    let sumResult =a+b;
+    return [sumResult,`The sum of ${a} and ${b} is ${sumResult}`;
+}
+ 
 
+// Here is the test for sum(); uncomment it to run it
+testSum(4, 7);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+/////////////////////////////////////
+/* Problem 2
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"The product of 5 and 9 is 45."
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+
+// Write your code here
+function multiply(a, b) { //eslint-disable-line
+let multi = a * b
+return [multi, `The product of ${a} and ${b} is ${multi}.`]
+}
+// Here is the test for multiply(); uncomment it to run it
+ testMultiply(5,9);
 // Here is the test for sumAndMultiply(); uncomment it to run it
 testSumAndMultiply(4,7,5);
 
